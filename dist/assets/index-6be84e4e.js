27eabbfe8dfb5ed1727e6abade91abf6899b43f7
xsm <@xushaomeng>
2025-06-23 14:44:58 +0800

    feat: build22

--- dist/assets/index-6be84e4e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,p as n,h as r}from"./index-f69cdbfa.js";const a=e({},[["render",function(e,a){return r(),n("h1",null,"Example")}]]);export{a as default};
---
+++ dist/assets/index-8ec47b8e.js
@@ -0,0 +1,1 @@
+import{_ as e,p as n,h as r}from"./index-62ae0c91.js";const a=e({},[["render",function(e,a){return r(),n("h1",null,"Example")}]]);export{a as default};
